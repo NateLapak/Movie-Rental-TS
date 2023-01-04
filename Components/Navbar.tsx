d5747e57a5a9e6c54b01f5afbd5f7fe6d24570a8
NateLapak <Nate.lapak12@gmail.com>
2023-01-03 18:05:42 -0700

    Day ten

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -21,12 +21,7 @@ const Navbar = () => {
                         </svg>
                     </button>
 
-                    {/* Searchbar */}
-                    <form>
-
-                    </form>
-                    
-                    <div className="hidden w-full md:block md:w-auto" id="navbar-default bg">
+                    <div className="w-full md:block md:w-auto" id="navbar-default bg">
                         <ul
                             className="flex flex-col p-4 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
